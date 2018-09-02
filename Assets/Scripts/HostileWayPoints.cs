17325d7f5ed78647e090771afbbaf18b4fd43d99
Rishaan Gupta <notanapachehelicopter@gmail.com>
2018-09-02 19:34:27 +1200

    WayPoints & HostileWayPoints

--- Assets/Scripts/HostileWayPoints.cs
+++ Assets/Scripts/HostileWayPoints.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WayPoints : MonoBehaviour {
+public class HostileWayPoints : MonoBehaviour {
 
     // put the points from unity interface
     public Transform[] wayPointList;
 
-    public bool rotate = true;
     public int currentWayPoint = 0;
     Transform targetWayPoint;
 
-    public float speed = 4f;
+
+    public int playerLayerID = 10;
+    public float speed = 1f;
+    public float knockbackSpeed = 4f;
+    public bool rotate= true;
+    public float damage = 20f;
 
     void Start() {
 
@@ -27,16 +31,25 @@ public class WayPoints : MonoBehaviour {
         }
     }
 
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col != null && col.gameObject.layer == playerLayerID)
+        {
+            Vector3 v = knockbackSpeed * Vector3.Normalize(transform.forward);
+            if (PlayerHealth.health >= 0f) PlayerHealth.health -= damage;
+            PlayerMovement.player.GetComponent<Rigidbody2D>().velocity = knockbackSpeed * Vector3.Normalize(transform.forward);
+            GetComponent<Rigidbody2D>().velocity = -v;
+        }
+    }
+
     void walk() {
         // rotate towards the target
-        if (rotate)
-        {
+        if (rotate) {
             Vector3 targetdiff = targetWayPoint.position - transform.position;
             targetdiff.x = transform.forward.x;
             targetdiff.y = transform.forward.y;
             transform.forward = Vector3.RotateTowards(transform.forward, targetdiff, speed * Time.deltaTime, 0.0f);
         }
-
         // move towards the target
         transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, speed * Time.deltaTime);
 
