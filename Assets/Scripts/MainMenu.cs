6484d00ea8609f81cd9b7780788bf5b91fba79e4
Feras Albaroudi <30617834+NeedsSoySauce@users.noreply.github.com>
2018-09-02 17:00:15 +1200

    Very basic main menu implemented

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string PlayScene; // Name of the scene that will be loaded when "Play" is clicked
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Load the first level
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(PlayScene);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Game quit");
+        Application.Quit();
+
+    }
+
+}
