885c282cb996081d20dd613179c0c993959fe0da
Feras Albaroudi <30617834+NeedsSoySauce@users.noreply.github.com>
2018-09-02 18:24:33 +1200

    Changed beartrap material

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public static GameObject[] scenes;
+
+    private void Awake()
+    {
+        // If a GameManager hasn't been instantiated yet, set the latest one as the only instance
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        } else if (instance != null)
+        {
+            Destroy(this);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
