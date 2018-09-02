d3d7833ce7426d310cc09d88cb1a2ff0b936a597
HexaCubist <zac@questionable.co.nz>
2018-09-02 12:11:07 +1200

    Update logic for player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float speedInit = 3f;
-	public static float speedMax = 1.5f; // Max speed. I don't know why this doesn't show up in the editor.
-	public static float speedMin = 0.2f;
-	public float acceleration = 0.5f;
-	public float deceleration = 0.25f;
-	
-	public static float speed;
-	string lastKey = "w";
+    public float speedInit = 2.2f;
+    public static float speedMax = 2.2f; // Max speed. I don't know why this doesn't show up in the editor.
+    public static float speedMin = 0.8f;
+    public float acceleration = 0.5f;
+    public float deceleration = 0.25f;
+
+    public static float speed;
+    string lastKey = "w";
+    static KeyCode[] strafekeys = new KeyCode[] { KeyCode.A, KeyCode.D };
+    static KeyCode[] walkkeys = new KeyCode[] { KeyCode.W, KeyCode.S };
 
     int xdir = 0;
     int ydir = 0;
@@ -24,11 +26,20 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        // Detect if user is pressing both A&D or W&S (opposites)
+        bool opposites = false;
+        if (Input.GetKey("w") && Input.GetKey("s") || Input.GetKey("a") && Input.GetKey("d")) {
+            opposites = true;
+        }
+
 		// If the user is holding down the same key as the last time, we should increase their speed
-		if (Input.GetKey(lastKey)) {
+		if (Input.GetKey(lastKey) && !opposites) {
+            Debug.Log(opposites);
             //Debug.Log("Accelerating");
             speed += acceleration * Time.deltaTime;
-		} else {
+		} else if(Input.anyKey) {
+            speed = speedMin;
+        } else {
             //Debug.Log("Decelerating");
 			speed -= deceleration * Time.deltaTime;
 		}
@@ -68,6 +79,6 @@ public class PlayerMovement : MonoBehaviour {
 
 		transform.Translate(x, y, 0);
 
-        //Debug.Log(speed);
+        // Debug.Log(speed);
 	}
 }
