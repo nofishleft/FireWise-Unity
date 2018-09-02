08e2812042054ca1da17c2ee5aca618d98a94268
Feras Albaroudi <30617834+NeedsSoySauce@users.noreply.github.com>
2018-09-03 03:04:58 +1200

    Game Victory Condition

--- Assets/Scripts/GameVictory.cs
+++ Assets/Scripts/GameVictory.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // This should be placed on a gameobject with a box collider 2d designating the target area
 // for the player to reach. This will cause the player to play a short animation before while fading to black.
 public class GameVictory : MonoBehaviour
 {
-    public float fadeDuration = 5f;
-    public GameObject fadePanel; // A that will gradually fade in from alpha 0 to alpha 255;
-
+    public float fadeDuration = 7f;
+    public Image fadePanel; // A panel that will gradually fade in from alpha 0 to alpha 255;
     public int playerLayerID = 10; // Layer ID of the player
+
+    float timeToWait = 0f;
+
+    private void Start()
+    {
+        timeToWait = fadeDuration;
+    }
 
     // If the player reaches the target area in a scene, they have won
     void OnCollisionEnter2D(Collision2D col)
@@ -18,12 +25,15 @@ public class GameVictory : MonoBehaviour
         if (col.gameObject.layer == playerLayerID)
         {
             Debug.Log("Player Victory");
-
-
-
-
-
-            GameManager.instance.Victory();
+            fadePanel.CrossFadeAlpha(255, fadeDuration, false);
+            if (timeToWait > 0)
+            {
+                timeToWait -= Time.deltaTime;
+                return;
+            } else
+            {
+                GameManager.instance.Victory();
+            } 
         }
     }
 }
